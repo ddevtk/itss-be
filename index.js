c0161fbdd2755d9aabb406f093b6f1079ebaf53f
ddevtk <buiducduong2342000@gmail.com>
2022-12-21 21:16:56 +0700

    fix cors

--- index.js
+++ index.js
@@ -51,7 +51,36 @@ mongoose
 app.use(express.json({ limit: MAX.SIZE_JSON_REQUEST }));
 app.use(express.urlencoded({ limit: MAX.SIZE_JSON_REQUEST }));
 app.use(cookieParser());
-app.use(cors(corsConfig));
+// app.use(cors(corsConfig));
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    app.get('env') !== 'production'
+      ? 'http://localhost:3000'
+      : process.env.CORS_ORIGIN,
+  );
+
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type',
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 // ================== Listening ... ==================
 app.listen(PORT, () => {
